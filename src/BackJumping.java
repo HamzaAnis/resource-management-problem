a0ed24cf2d3faa63a264688619788e5e5a527ef1
Hamza Anis <hamzaanis9514@gmail.com>
2018-01-03 22:04:17 +0500

    back jumping

--- src/BackJumping.java
+++ src/BackJumping.java
@@ -24,7 +24,6 @@ public class BackJumping {
 	// this is not implemented from runable
 	public void run() {
 		office.seperateMeetings();
-		int sum = 0;
 		int roomIndex = 0;
 		int day = 1;
 		Time time = office.getOfficeTimings().getOpening();
@@ -54,11 +53,61 @@ public class BackJumping {
 					office.getMWP().get(k).setRoomAllocated(true);
 					office.getMWP().get(k).setRoom(office.getRWP().get(roomIndex));
 
-					office.getMWP().get(k).setTime(office.getRWP().get(roomIndex).getFreeTime());;
-					office.getRWP().get(roomIndex).setFreeTime( office.getRWP().get(roomIndex).getFreeTime().addDuration(office.getMWP().get(k).getDuration()));
-					office.getMWP().get(k).setDay(day);;
+					office.getMWP().get(k).setTime(office.getRWP().get(roomIndex).getFreeTime());
+
+					office.getRWP().get(roomIndex).setFreeTime(office.getRWP().get(roomIndex).getFreeTime()
+							.addDuration(office.getMWP().get(k).getDuration()));
+					office.getMWP().get(k).setDay(day);
+
 				}
 			}
+		} else {
+			System.out.println("Your office don't have any room with projector!\\n\\n");
+		}
+
+		// *******************************
+		// * Meetings without projectors *
+		// *******************************
+
+		sum = 0;
+		roomIndex = 0;
+		day = 1;
+		time = office.getOfficeTimings().getOpening();
+
+		if (office.getRWOP().size() > 0) {
+			for (int i = 0; i < office.getMWOP().size(); i++)
+				sum += office.getMWOP().get(i).getDuration().getminute();
+
+			for (int k = 0; k < office.getMWOP().size(); k++) {
+				if (!office.getMWOP().get(k).isRoomAllocated()) {
+					if (roomIndex >= office.getRWOP().size())
+						roomIndex = 0;
+					day++;
+					office.FreeAllRooms();
+
+					time = office.getRWOP().get(roomIndex).getFreeTime()
+							.addDuration(office.getMWOP().get(k).getDuration());
+
+					if (time.getMinutes() > office.getOfficeTimings().getClosing().getMinutes()) {
+						roomIndex++;
+						if (roomIndex >= office.getRWOP().size())
+							roomIndex = 0;
+						day++;
+						office.FreeAllRooms();
+					}
+
+					office.getMWOP().get(k).setRoomAllocated(true);
+					office.getMWOP().get(k).setRoom(office.getRWOP().get(roomIndex));
+
+					office.getMWOP().get(k).setTime(office.getRWOP().get(roomIndex).getFreeTime());
+					office.getRWOP().get(roomIndex).setFreeTime(office.getRWOP().get(roomIndex).getFreeTime());
+					office.getRWOP().get(roomIndex).setFreeTime(office.getRWOP().get(roomIndex).getFreeTime()
+							.addDuration(office.getMWOP().get(k).getDuration()));
+					office.getMWOP().get(k).setDay(day);
+				}
+			}
+		} else {
+			System.out.println("Your office don't have any simple!\\n\\n");
 		}
 
 	}
